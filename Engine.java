7558d3ee1e4f414322b84e65988522710de33076
Linh Pham <lpham@smith.edu>
2023-03-06 00:52:41 -0500

    initial commits

--- Engine.java
+++ Engine.java
@@ -1,3 +1,42 @@
 public class Engine {
 
+    FuelType fuelType;
+    double maxFuel;
+    double currentFuel;
+
+    public Engine(FuelType fuelType, double currentFuel) {
+        this.fuelType = fuelType;
+        this.maxFuel = 100;
+        this.currentFuel = currentFuel;
+    }
+
+    public void refuel(){
+        if (this.currentFuel == this.maxFuel) { // Already enrolled
+            throw new RuntimeException("The fuel level is already at max");
+        }
+        this.currentFuel = this.maxFuel;
+
+    }
+
+    public void go(){
+        if (this.currentFuel == 0) { // Already enrolled
+            throw new RuntimeException("Insufficient fuel level, needs refuel ASAP.");
+        }
+        this.currentFuel -= 10;
+        System.out.println("The train is running. The remaining fuel level is " + this.currentFuel);
+
+
+    }
+
+    public static void main(String[] args) {
+        Engine myEngine = new Engine(FuelType.ELECTRIC, 40.0);
+        try {
+            while (true) {
+                myEngine.go();
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage()); // Out of fuel
+        }
+    }
+
 }
--- Passenger.java
+++ Passenger.java
@@ -1,8 +1,26 @@
 public class Passenger {
     
-    private String name;
+    String name;
 
     public Passenger(String name) {
         this.name = name;
     }
+
+    public void boardCar(Car c){
+        try {
+            c.addPassenger(this);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void getOffCar(Car c){
+        try {
+            c.removePassenger(this);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
 }
+
